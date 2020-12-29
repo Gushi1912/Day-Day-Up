b9d509179afc23adaed1deac3db97d71b04a1797
Gushiyang <Gushi1912@foxmail.com>
2020-12-29 18:11:17 +0800

    修改逻辑思维导图

--- Day-Day-Study/jdk1.8-Time/src/com/gushi/time/TimeDemo2.java
+++ Day-Day-Study/jdk1.8-Time/src/com/gushi/time/TimeDemo2.java
@@ -1,9 +1,10 @@
 package com.gushi.time;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
 
 /**
  * @author Gushi1912
@@ -12,8 +13,13 @@ import java.time.format.DateTimeFormatter;
 public class TimeDemo2 {
     public static void main(String[] args) {
 //        DateTimeFormatter formatter = new DateTimeFormatter();
-        localDateTime();
+//        localDateTime();
+//        dateTimeFormatter();
+//        timeOperation();
+//
+//        timeTest();  转换有些问题
 
+        durationAndPeriod();
     }
 
     //LocalDateTime获取时间的方法
@@ -26,5 +32,55 @@ public class TimeDemo2 {
         System.out.println(now3);
 
 //        LocalDateTime.of
+    }
+
+    /**
+     * 时间转换
+     */
+    public static void dateTimeFormatter() {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        String time = dateTimeFormatter.format(LocalDateTime.now());
+        System.out.println(time);
+    }
+
+    /**
+     * 时间加减操作
+     */
+    public static void timeOperation() {
+        LocalDateTime time = LocalDateTime.of(2019, 12, 23, 11, 22, 33);
+        System.out.println(time);
+        LocalDateTime localDateTime = time.plusDays(9).minusHours(4);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String format = dateTimeFormatter.format(localDateTime);
+        System.out.println(format);
+    }
+
+    /**
+     * 使用withXXXX操作时间和日期
+     */
+    public static void withTime() {
+        LocalDateTime time = LocalDateTime.of(2015, 8, 23, 13, 14, 52);
+        LocalDateTime localDateTime = time.withDayOfMonth(3).withMonth(12).withHour(12).withMinute(23).withSecond(44);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String format = dateTimeFormatter.format(localDateTime);
+        System.out.println(format);
+
+        LocalDateTime dateTime = time.with(TemporalAdjusters.firstDayOfMonth());
+//        dateTime.isAfter()
+    }
+
+    public static void timeTest() {
+        String s = new String("2020-12-26 11:22:33 +08:00");
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime parse = LocalDateTime.parse(s, dateTimeFormatter);
+        System.out.println(parse);
+    }
+
+    public static void durationAndPeriod() {
+        LocalDateTime time1 = LocalDateTime.of(2020, 12, 12, 12, 12, 12);
+        LocalDateTime time2 = LocalDateTime.of(2020, 12, 13, 14, 14, 34);
+        Duration between = Duration.between(time1, time2);
+//        Period.between()
+        System.out.println(between);
     }
 }
---
+++ Day-Day-Study/jdk1.8-Time/src/com/gushi/time/TimeDemo3.java
@@ -0,0 +1,77 @@
+package com.gushi.time;
+
+/**
+ * @author Gushi1912 | gushiyang@sheca.com
+ * @version 0.0.1
+ * 2020/12/29
+ */
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Set;
+
+/**
+ * ZonedDateTime的测试，它就相当于LocalDateTime和ZoneId结合的产物
+ * LocalDateTime是不带时区的，ZonedDateTime是带时区的
+ */
+public class TimeDemo3 {
+    public static void main(String[] args) {
+//        test1();
+//        test2();
+//        test3();
+        ZonedDateTime now = ZonedDateTime.now(ZoneId.systemDefault());
+        LocalDateTime localDateTime = now.toLocalDateTime();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss", Locale.US);
+        String format = formatter.format(localDateTime);
+        System.out.println(format);
+        Instant instant = Instant.now();
+        System.out.println(instant);
+        System.out.println(instant.getEpochSecond());
+//        System.out.println(instant.getLong());
+        System.out.println(instant.toEpochMilli());
+        System.out.println(instant.getNano());
+    }
+
+    /**
+     * ZonedDateTime时间的创建
+     */
+    public static void test1() {
+        ZonedDateTime time1 = ZonedDateTime.now();
+        ZonedDateTime time2 = ZonedDateTime.now(ZoneId.of("America/New_York"));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss", Locale.CHINA);
+//        String format = formatter.format(time1);
+//        System.out.println(format);
+        System.out.println(formatter.format(time1));
+        System.out.println(formatter.format(time2));
+        System.out.println(time1);
+        System.out.println(time2);
+    }
+
+    /**
+     * LocalDateTime和ZoneId结合就可以转换成ZonedDateTime
+     */
+    public static void test2() {
+        LocalDateTime localDateTime = LocalDateTime.of(2019,12,26,12,12,12);
+        ZonedDateTime zonedDateTime = localDateTime.atZone(ZoneId.systemDefault());
+        ZonedDateTime zonedDateTime1 = localDateTime.atZone(ZoneId.of("America/New_York"));
+        System.out.println(zonedDateTime1);
+        System.out.println(zonedDateTime);
+        Set<String> availableZoneIds = ZoneId.getAvailableZoneIds();
+        System.out.println(availableZoneIds);
+    }
+
+    /**
+     * 时区转换
+     */
+    public static void test3() {
+        ZonedDateTime time1 = ZonedDateTime.now(ZoneId.of("Asia/Shanghai"));
+        ZonedDateTime time2 = time1.withZoneSameInstant(ZoneId.of("America/New_York"));
+        System.out.println(time1);
+        System.out.println(time2);
+
+    }
+}
