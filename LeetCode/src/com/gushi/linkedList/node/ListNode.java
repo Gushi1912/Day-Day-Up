5f6c921dba8d55df6fda32ae3a6a4579f62e64af
Gushiyang <Gushi1912@foxmail.com>
2021-01-27 12:30:23 +0800

    力扣002

---
+++ LeetCode/src/com/gushi/linkedList/node/ListNode.java
@@ -0,0 +1,20 @@
+package com.gushi.linkedList.node;
+
+/**
+ * @author Gushi1912 | gushiyang@sheca.com
+ * @version 0.0.1
+ * 2021/1/26
+ */
+public class ListNode {
+    public int value;
+    public ListNode next;
+
+
+    public ListNode(int value, ListNode next) {
+        this.value = value;
+        this.next = next;
+    }
+
+    public ListNode() {
+    }
+}
